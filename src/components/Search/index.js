e10eb18029ebdc33ea941b4e1982d84887417260
Nelson Rojas <nelson_rojas_n@hotmail.com>
2022-12-11 23:25:56 -0300

    Corrección menor en selector y botón de búsqueda

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import Selector from "../Selector";
 import SearchButton from "../SearchButton";
 import InfoFiltros from "../InfoFiltros";
-import { useAllRazas } from "../../hooks/useAllRazas";
+import {useAllRazas} from "../../hooks/useAllRazas";
 
 function Search() {
   const razas = useAllRazas();
@@ -12,22 +12,25 @@ function Search() {
   const [selectedSubRaza, setSelectedSubRaza] = useState("");
   const [selectedFilter, setSelectedFilter] = useState("");
 
+  
   useEffect(() => {
     //actualizar filtro seleccionado
     const selectedFilter =
       selectedRaza + (selectedSubRaza.length > 0 ? "/" + selectedSubRaza : "");
     setSelectedFilter(selectedFilter);
+    
   }, [selectedRaza, selectedSubRaza]);
 
   useEffect(() => {
     //actualizar lista de subrazas
     setSelectedSubRaza("");
+    setSubRazas([]);
 
-    const subRazas = razas.find((item) => {
-      item.nombre === selectedRaza;
+    const subRazas = razas.filter((item) => {
+      return item.nombre === selectedRaza;
     });
+    subRazas.length > 0 && setSubRazas(subRazas[0].items);
 
-    subRazas && subRazas.length > 0 && setSubRazas(subRazas[0].items);
   }, [selectedRaza, razas]);
 
   return (
